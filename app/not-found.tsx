f89746a9872280f583a69a52131bf6aef035d676
Albert735 <03jayteee@gmail.com>
2024-11-06 12:32:19 +0000

    "Added 'use client' directive and replaced simple 'NotFound' text with an image component in NotFound.tsx"

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,7 +1,21 @@
+"use client";
+
 import React from "react";
+import Image from "next/image";
 
 const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <div className="h-[80dvh] w-full flex flex-col justify-center items-center">
+      <Image
+        src="/assets/404 error with people holding the numbers-bro.svg"
+        alt="Page not found"
+        className="h-[40rem] w-[40rem] object-contain"
+        priority
+        width={500}
+        height={500}
+      />
+    </div>
+  );
 };
 
 export default NotFound;
